93764968e11fdb10554d85a6b3427c2b014bd5b3
shreyash <shreyashv339@gmailcom>
2026-03-05 07:48:47 +0000

    "first commit"

---
+++ node-jenkins-project/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Jenkins CI/CD Project");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+module.exports = app;
